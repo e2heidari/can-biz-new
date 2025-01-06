b095d7fb0bbb652a5f6f9f69f959453ae80a7ad9
Ehsan_Heidari <ehsan.heydari@gmail.com>
2025-01-05 21:33:12 -0800

    fix date issue

--- src/services/businesses-data.ts
+++ src/services/businesses-data.ts
@@ -3,6 +3,6 @@ import data from "./businesses-data.json";
 
 export const businesses: Business[] = data.map((business) => ({
   ...business,
-  createdAt: new Date(),
-  updatedAt: new Date(),
+  createdAt: new Date(business.createdAt),
+  updatedAt: new Date(business.updatedAt),
 }));
