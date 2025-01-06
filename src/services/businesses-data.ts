35033bf978af637d26046e04c6a5d0ab408bfe81
Ehsan_Heidari <ehsan.heydari@gmail.com>
2025-01-05 21:21:18 -0800

    fixed data

--- src/services/businesses-data.ts
+++ src/services/businesses-data.ts
@@ -5,11 +5,4 @@ export const businesses: Business[] = data.map((business) => ({
   ...business,
   createdAt: new Date(),
   updatedAt: new Date(),
-  socialMedia: {
-    ...business.socialMedia,
-    instagram: {
-      ...business.socialMedia.instagram,
-      followers: Number(business.socialMedia.instagram.followers), // Convert followers to number
-    },
-  },
 }));
